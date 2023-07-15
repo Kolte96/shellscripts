174f2d0326f1a21769eb0d4d0b6b3b20efb95f32
root <root@ip-172-31-44-67.ap-south-1.compute.internal>
2023-07-15 17:05:01 +0000

    1st commit

--- Script1.sh
+++ Script1.sh
@@ -4,7 +4,5 @@ echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Hello batch-19, We are learning Develops"
-echo 'Hello'
-echo 'hhjoj'
-echo 'Hello'
-
+echo 'Hello Apurva'
+echo 'HI'
